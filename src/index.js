176c4dd7f3d2015fd900261663871d56df627f3f
Selim Imoberdorf <selimob@gmail.com>
2020-03-27 00:21:48 +0100

    Update starter with files by slimiles
    - Own CSS Structure
    - New dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 /**
  * This file kicks off Webpack's tasks to minify and compile CSS and JS.
  */
-import './styles/main.scss';
-import './scripts/bootstrap-carousel';
-
-/**
- * Bootstrap carousel standalone from https://codepen.io/derekjp/pen/QjmxdK
- */
-$('.carousel').carousel({
-    interval: 0
-});
+import "./styles/app.scss";
